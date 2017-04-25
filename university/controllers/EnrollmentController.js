254f8bd6f4562992ab95d6ebe718a85b511a18e0
Yousef Shanawany <askyous@gmail.com>
2017-04-24 19:38:31 -0700

    Added university

---
+++ university/controllers/EnrollmentController.js
@@ -0,0 +1,11 @@
+/**
+ * EnrollmentController
+ *
+ * @description :: Server-side logic for managing Enrollments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
